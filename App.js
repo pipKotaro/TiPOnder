59a05bb0bd1f6781d82945f46b1ab651ff823eb3
TAiki <b1016164@gmail.com>
2018-09-05 11:11:18 +0900

    フラッシュできた

--- App.js
+++ App.js
@@ -1,62 +1,107 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import { View, Modal, Image, CameraRoll,TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
-export default class App extends React.Component {
+import React, { Component } from 'react';
+import { Container, Header, Content, Button, Text, Fab ,Icon} from 'native-base';
+//fabで円のアクション系を書く
+
+
+export default class CameraView extends React.Component {
   state = {
-    isLoadingComplete: false,
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,//カメラの内カメとか,いんかめと
+    photo: null,
+    flashMode:Camera.Constants.FlashMode.off
   };
 
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  setPhoto(photo) {
+    this.setState({ photo: photo });
+  }
+
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+
       return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type} flashMode={this.state.flashMode}  ref={cam => { this.camera = cam; }} />
+
+
+                  <Fab
+                     active={this.state.active}
+                     direction="up"
+                     containerStyle={{ }}
+                     style={{ backgroundColor: '#5067FF' }}
+                     position="bottomRight"
+                     onPress={photo => {
+                       if (this.camera) {
+                         this.camera.takePictureAsync().then(photo => {
+                           // カメラオブジェクト取得
+                           this.setPhoto(photo);
+                           // 三秒後にモーダルを閉じる
+                           setTimeout(() => {
+                             this.setPhoto(null);
+                             // 写真を保存
+                             CameraRoll.saveToCameraRoll(photo.uri);
+                           }, 3000);
+                         });
+                       }
+                     }}>
+                     <Icon name="share" />
+                     <Button style={{ flex: 1 ,marginbottom: 30,}} title="snap!"  />
+                   </Fab>
+
+                   <Fab
+                      active={this.state.active}
+                      direction="up"
+                      containerStyle={{ }}
+                      style={{ backgroundColor: '#a9f43e' }}
+                      position="bottomLeft"
+                      onPress={photo => {
+                        this.setState({
+                          type: this.state.type === Camera.Constants.Type.back
+                          ? Camera.Constants.Type.front
+                          : Camera.Constants.Type.back,
+                          });
+                      }}>
+                      <Icon name="share" />
+                      <Button style={{ flex: 1 ,marginbottom: 30,}} title="snap!"  />
+                    </Fab>
+
+
+                    <Fab
+                       active={this.state.flashMode}
+                       direction="up"
+                       containerStyle={{ }}
+                       style={{ backgroundColor: '#f0e94f' }}
+                       position="topLeft"
+                       onPress={photo => {
+                         this.setState({
+                           flashMode: this.state.flashMode == Camera.Constants.FlashMode.off
+                           ? Camera.Constants.FlashMode.on
+                           : Camera.Constants.FlashMode.off,
+                           });
+                       }}>
+                       <Icon name="share" />
+                       <Button style={{ flex: 1 ,marginbottom: 30,}} title="snap!"  />
+                     </Fab>
+
+
+
+          <Modal
+            animationType="slide"
+            visible={this.state.photo !== null}
+            onRequestClose={() => {
+              alert('Modal has been closed.');
+            }}>
+            <Image style={{width: "100%", height: "100%" }}
+              source={{ uri: this.state.photo === null ? "" : this.state.photo.uri }}/>
+          </Modal>
+
         </View>
       );
     }
-  }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
